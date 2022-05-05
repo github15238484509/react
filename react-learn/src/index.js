3703d5162173547debf1fe97d92d6d866532de35
刘梦岗 <1138075190@qq.com>
2022-05-06 07:52:14 +0800

    传递元素内容

--- react-learn/src/index.js
+++ react-learn/src/index.js
@@ -1,29 +1,29 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import src1 from "./assets/1.gif";
-import src2 from "./assets/2.webp";
-import src3 from "./assets/3.webp";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-let arr = [src1, src2, src3];
-let index = 0
-function start() {
-  setInterval(() => {
-    index = (index + 1) % 3
-    render()
-  }, 1000)
+function App(props) {
+  console.log(props);
+  return (<div>
+    App的内容
+    <p>
+      {props.children}
+    </p>
+  </div>)
 }
-function render() {
-  let img = <img src={arr[index]}></img>
-  root.render(
-    <React.StrictMode>
-      {img}
-    </React.StrictMode>
-  );
-}
-start()
+
+
+
+
+root.render(
+  <React.StrictMode>
+    <App content="这是content的内容" children="789" className="465">传入的文字</App>
+  </React.StrictMode>
+);
+
 
 
 
