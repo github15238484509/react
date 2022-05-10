85e667a4049410b13f6ec0156716f860997b16ff
刘梦岗 <1138075190@qq.com>
2022-05-10 13:50:05 +0800

    旧的context，不可以用了

--- react-learn/src/index.js
+++ react-learn/src/index.js
@@ -1,29 +1,76 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import src1 from "./assets/1.gif";
-import src2 from "./assets/2.webp";
-import src3 from "./assets/3.webp";
+import PropTypes from 'prop-types'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-let arr = [src1, src2, src3];
-let index = 0
-function start() {
-  setInterval(() => {
-    index = (index + 1) % 3
-    render()
-  }, 1000)
+class A extends React.Component {
+  state = {
+    a: '我是a的内容'
+  }
+  constructor(props) {
+    super(props)
+    console.log("...");
+  }
+  static childContextTypes = {
+    a:PropTypes.string
+  }
+  getChildContext=()=>{
+    return {
+      a:"87465"
+    }
+  }
+  change = () => {
+    this.setState({
+      a: `我是a的内容我变心了${Math.random()}`
+    })
+  }
+  render() {
+    return (
+      <>
+        <h2>A</h2>
+        <B></B>
+      </>
+    )
+  }
 }
-function render() {
-  let img = <img src={arr[index]}></img>
-  root.render(
-    <React.StrictMode>
-      {img}
-    </React.StrictMode>
-  );
+
+class B extends React.Component {
+  static contextType = {
+    a: PropTypes.string
+  }
+  constructor(props, context) {
+    super(props)
+    console.log(context);
+  }
+  render() {
+    console.log(this);
+    return (
+      <>
+        <h2>B:{this.context.a}</h2>
+        <C></C>
+      </>
+    )
+  }
 }
-start()
+
+class C extends React.Component {
+  render() {
+
+    return (
+      <h2>C</h2>
+    )
+  }
+}
+
+
+root.render(
+  <React.StrictMode>
+    <A></A>
+  </React.StrictMode>
+);
+
 
 
 
