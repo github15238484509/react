00fae045e44b23719d34d24e249b8d41d86de535
刘梦岗 <1138075190@qq.com>
2022-05-11 11:02:42 +0800

    Portal的练习

--- react-learn/src/index.js
+++ react-learn/src/index.js
@@ -1,13 +1,19 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import ReactDOM1 from 'react-dom';
 import FromTest from './component/From/test';
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-
+let A = () => {
+  return (ReactDOM1.createPortal(
+    <React.StrictMode>
+      <span>  456</span>
+    </React.StrictMode>, document.getElementById('root1')))
+}
 root.render(
   <React.StrictMode>
     <FromTest></FromTest>
+    <A></A>
   </React.StrictMode>
 );
 
