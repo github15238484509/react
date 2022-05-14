3ce38727d12becbad429c545f5c747d81bbfe1b5
刘梦岗 <1138075190@qq.com>
2022-05-14 12:13:28 +0800

    使用useContext钩子函数

--- react-learn/src/app.js
+++ react-learn/src/app.js
@@ -1,58 +1,47 @@
-import React, { useState } from 'react'
-// import React, { useState, useReducer } from 'react'
+import React, { useState, createContext, useContext } from 'react'
 
+let txt = createContext()
+// 元素方式
+// function Test() {
+//     return (
+//         <txt.Consumer>
+//             {
+//                 (value) => (
+//                     <span> test {value}</span>
+//                 )
+//             }
+//         </txt.Consumer>
+//     )
+// }
 
-/**
- * 这是一个reducer通用函数
- * @param {Function} reducer  处理函数
- * @param {any} initData 初始值
- * @param {Function} initFun 参数初始值的函数，可以不写
- * @returns {Array} [state,dispath]
- */
+//静态方法方式
+// class Test extends React.Component {
+//     static contextType = txt
+//     render() {
+//         console.log(this);
+//         return (
+//             <>
+//                 <span> test {this.context}</span>
+//             </>
+//         )
+//     }
+// }
 
-function useReducer(reducer, initData, initFun) {
-    let [state, setState] = useState(initFun ? initFun(initData) : initData)
-    /**
-     * 
-     * @param {Object} action 213
-     */
-    function dispatch(action) {
-        let newState = reducer(state, action)
-        console.log(`状态 ${state} --> ${newState}`);
-        setState(newState)
-    }
-    return [state, dispatch]
-}
-
-function reducer(state, action) {
-    switch (action.type) {
-        case "increase":
-            return state - action.payload
-        case "decrease":
-            return state + action.payload
-        default:
-            return state
-    }
-}
-
-
-export default function App() {
-    const [state, dispatch] = useReducer(reducer, 10, (params) => { return params * 2 })
+// 使用钩子函数的方式
+function Test(prop) {
+    let value = useContext(txt)
     return (
         <>
-            <button onClick={() => {
-                dispatch({
-                    type: "increase",
-                    payload: 10
-                })
-            }}>-</button>
-            <span>{state}</span>
-            <button onClick={() => {
-                dispatch({
-                    type: "decrease",
-                    payload: state + 1
-                })
-            }}>+</button>
+            <span> test {value}</span>
+        </>
+    )
+}
+export default function App() {
+    return (
+        <>
+            <txt.Provider value={"这是上下文"}>
+                <Test></Test>
+            </txt.Provider>
         </>
     )
 }
