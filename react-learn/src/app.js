d83cb8c7d4d855ed61dacadef7dcb47566b66a1f
刘梦岗 <1138075190@qq.com>
2022-05-17 09:46:52 +0800

    CSSTransition的练习

--- react-learn/src/app.js
+++ react-learn/src/app.js
@@ -1,57 +1,30 @@
 import React, { useState } from 'react'
-import { Transition,CSSTransition } from "react-transition-group"
+import { Transition, CSSTransition } from "react-transition-group"
 import "./app.css"
 
 
-const duration = 300;
-
-const defaultStyle = {
-    transition: `opacity ${duration}ms ease-in-out`,
-    opacity: 0,
+function Comp1({ visibility }) {
+    return <CSSTransition appear mountOnEnter in={visibility} timeout={1000}>
+        <h1 className='title'>Comp1</h1>
+    </CSSTransition>
+}
+function Comp2({ visibility }) {
+    return <CSSTransition appear mountOnEnter in={visibility} timeout={1000}>
+        <h1 className='title'>Comp2</h1>
+    </CSSTransition>
 }
 
-const transitionStyles = {
-    entering: { opacity: 1 },
-    entered: { opacity: 1 },
-    exiting: { opacity: 0 },
-    exited: { opacity: 0 },
-};
-// export default function App() {
-//     let [boolean, setBoolean] = useState(false)
-
-//     return (
-//         <>
-//             <Transition mountOnEnter in={boolean} timeout={duration}>
-//                 {state => {
-//                     console.log(state);
-//                     return (
-//                         <div style={{
-//                             ...defaultStyle,
-//                             ...transitionStyles[state]
-//                         }}>
-//                             I'm a fade Transition!
-//                         </div>
-//                     )
-//                 }}
-//             </Transition>
-//             <button onClick={() => {
-//                 setBoolean(!boolean)
-//             }}>toggle</button>
-//         </>
-//     )
-// }
 export default function App() {
-    const [inProp, setInProp] = useState(false);
+    const [visibility, setVisibility] = useState(true);
 
     return (
         <div>
-            <CSSTransition in={inProp} timeout={2000} classNames="my-node">
-                <div>
-                    {"I'll receive my-node-* classes"}
-                </div>
-            </CSSTransition>
-            <button type="button" onClick={() => setInProp(true)}>
-                Click to Enter
+            <div className='container'>
+                <Comp1 visibility={visibility}></Comp1>
+                <Comp2 visibility={!visibility}></Comp2>
+            </div>
+            <button type="button" onClick={() => setVisibility(!visibility)}>
+                toggle
             </button>
         </div>
     );
