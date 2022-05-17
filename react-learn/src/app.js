46e8af96d81ac00fc55190511b95d9993541bf87
刘梦岗 <1138075190@qq.com>
2022-05-17 08:42:23 +0800

    tansition-group小练习

--- react-learn/src/app.js
+++ react-learn/src/app.js
@@ -1,58 +1,58 @@
 import React, { useState } from 'react'
-// import React, { useState, useReducer } from 'react'
+import { Transition,CSSTransition } from "react-transition-group"
+import "./app.css"
 
 
-/**
- * 这是一个reducer通用函数
- * @param {Function} reducer  处理函数
- * @param {any} initData 初始值
- * @param {Function} initFun 参数初始值的函数，可以不写
- * @returns {Array} [state,dispath]
- */
+const duration = 300;
 
-function useReducer(reducer, initData, initFun) {
-    let [state, setState] = useState(initFun ? initFun(initData) : initData)
-    /**
-     * 
-     * @param {Object} action 213
-     */
-    function dispatch(action) {
-        let newState = reducer(state, action)
-        console.log(`状态 ${state} --> ${newState}`);
-        setState(newState)
-    }
-    return [state, dispatch]
+const defaultStyle = {
+    transition: `opacity ${duration}ms ease-in-out`,
+    opacity: 0,
 }
 
-function reducer(state, action) {
-    switch (action.type) {
-        case "increase":
-            return state - action.payload
-        case "decrease":
-            return state + action.payload
-        default:
-            return state
-    }
-}
+const transitionStyles = {
+    entering: { opacity: 1 },
+    entered: { opacity: 1 },
+    exiting: { opacity: 0 },
+    exited: { opacity: 0 },
+};
+// export default function App() {
+//     let [boolean, setBoolean] = useState(false)
 
-
+//     return (
+//         <>
+//             <Transition mountOnEnter in={boolean} timeout={duration}>
+//                 {state => {
+//                     console.log(state);
+//                     return (
+//                         <div style={{
+//                             ...defaultStyle,
+//                             ...transitionStyles[state]
+//                         }}>
+//                             I'm a fade Transition!
+//                         </div>
+//                     )
+//                 }}
+//             </Transition>
+//             <button onClick={() => {
+//                 setBoolean(!boolean)
+//             }}>toggle</button>
+//         </>
+//     )
+// }
 export default function App() {
-    const [state, dispatch] = useReducer(reducer, 10, (params) => { return params * 2 })
+    const [inProp, setInProp] = useState(false);
+
     return (
-        <>
-            <button onClick={() => {
-                dispatch({
-                    type: "increase",
-                    payload: 10
-                })
-            }}>-</button>
-            <span>{state}</span>
-            <button onClick={() => {
-                dispatch({
-                    type: "decrease",
-                    payload: state + 1
-                })
-            }}>+</button>
-        </>
-    )
+        <div>
+            <CSSTransition in={inProp} timeout={2000} classNames="my-node">
+                <div>
+                    {"I'll receive my-node-* classes"}
+                </div>
+            </CSSTransition>
+            <button type="button" onClick={() => setInProp(true)}>
+                Click to Enter
+            </button>
+        </div>
+    );
 }
