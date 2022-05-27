5daf13bbe78c2c29f83e3e7380ed4c4c137268c9
刘梦岗 <1138075190@qq.com>
2022-05-27 20:21:31 +0800

    bindActionCreators

---
+++ react-learn/src/redux/action/action-dispatch.js
@@ -0,0 +1,13 @@
+import * as actionTypes from "./action-types"
+
+
+export function getIncrease() {
+    return {
+        type: actionTypes.INCREASE
+    }
+}
+export function getDecrease() {
+    return {
+        type: actionTypes.DECREASE
+    }
+}
--- react-learn/src/redux/index.js
+++ react-learn/src/redux/index.js
@@ -1,20 +1,26 @@
 
-import { createStore } from "redux"
+import { createStore, bindActionCreators } from "redux"
+import * as actionTypes from "./action/action-types"
+import * as actionDispatch from "./action/action-dispatch"
 function reducer(state, action) {
-    if (action.type === "increase") {
+    if (action.type === actionTypes.INCREASE) {
         return state + 1
-    } else if (action.type === "decrease") {
+    } else if (action.type === actionTypes.DECREASE) {
         return state - 1
     }
     return state
 }
 let store = createStore(reducer, 10)
-console.log(store.getState());
 store.subscribe(() => {
     console.log('store改变了');
 })
-store.dispatch({
-    type: 'increase'
-})
+console.log(store.getState());
+
+let dispatch = bindActionCreators(actionDispatch, store.dispatch)
+console.log(dispatch, actionDispatch);
+dispatch.getIncrease()
+// store.dispatch({
+//     type: actionTypes.INCREASE
+// })
 console.log(store.getState());
 console.log(store);
